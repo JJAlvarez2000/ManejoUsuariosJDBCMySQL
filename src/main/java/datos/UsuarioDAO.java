1ac7f57fd06609139d661364eb0d88e4b0d585a3
Julio <alvarezjjoel6@gmail.com>
2022-11-14 14:40:32 -0300

    Se agrego manejo de transacciones

--- src/main/java/datos/UsuarioDAO.java
+++ src/main/java/datos/UsuarioDAO.java
@@ -1,2 +1,129 @@
-package datos;public class UsuarioDAO {
+package datos;
+
+import domain.Usuario;
+
+import java.sql.*;
+import java.util.*;
+
+public class UsuarioDAO {
+
+    private Connection conexionTransaccional;
+    private static final String SQL_SELECT = "SELECT id_usuario, usuario, password FROM usuario";
+    private static final String SQL_INSERT = "INSERT INTO usuario(usuario, password) VALUES(?, ?)";
+    private static final String SQL_UPDATE = "UPDATE usuario SET usuario = ?, password = ? WHERE id_usuario = ?";
+    private static final String SQL_DELETE = "DELETE FROM usuario WHERE id_usuario = ?";
+
+    public UsuarioDAO() {
+
+    }
+    public UsuarioDAO(Connection conexionTransaccional) {
+        this.conexionTransaccional = conexionTransaccional;
+    }
+
+    public List<Usuario> seleccionar() throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        Usuario user = null;
+        List<Usuario> usuarios = new ArrayList<>();
+
+        try {
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_SELECT);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                int idUsuario = rs.getInt("id_usuario");
+                String usuario = rs.getString("usuario");
+                String password = rs.getString("password");
+                user = new Usuario(idUsuario, usuario, password);
+                usuarios.add(user);
+            }
+        } finally {
+            try {
+                Conexion.close(rs);
+                Conexion.close(stmt);
+                if (this.conexionTransaccional == null) {
+                    Conexion.close(conn);
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace(System.out);
+            }
+        }
+        return usuarios;
+    }
+
+    public int insertar(Usuario usuario) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+        try {
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_INSERT);
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getPassword());
+            System.out.println("Ejecutando query: " + SQL_INSERT);
+            rows = stmt.executeUpdate();
+            System.out.println("Registros afectados: " + rows);
+        } finally {
+            try {
+                Conexion.close(stmt);
+                if (this.conexionTransaccional == null) {
+                    Conexion.close(conn);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+        return rows;
+    }
+
+    public int update(Usuario usuario) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+        try {
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_UPDATE);
+            stmt.setString(1, usuario.getUsuario());
+            stmt.setString(2, usuario.getPassword());
+            stmt.setInt(3, usuario.getIdUsuario());
+            System.out.println("Ejecutando query: " + SQL_UPDATE);
+            rows = stmt.executeUpdate();
+            System.out.println("Registros afectados: " + rows);
+        } finally {
+            try {
+                Conexion.close(stmt);
+                if (this.conexionTransaccional == null) {
+                    Conexion.close(conn);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return rows;
+    }
+
+    public int delete(Usuario usuario) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        int rows = 0;
+        try {
+            conn = this.conexionTransaccional != null ? this.conexionTransaccional : Conexion.getConnection();
+            stmt = conn.prepareStatement(SQL_DELETE);
+            stmt.setInt(1, usuario.getIdUsuario());
+            System.out.println("Ejecutando query: " + SQL_DELETE);
+            rows = stmt.executeUpdate();
+        } finally {
+            try {
+                Conexion.close(stmt);
+                if (this.conexionTransaccional == null) {
+                    Conexion.close(conn);
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return rows;
+    }
 }
--- src/test/java/TestManejoUsuarios.java
+++ src/test/java/TestManejoUsuarios.java
@@ -1,2 +1,39 @@
-package PACKAGE_NAME;public class TestManejoUsuarios {
+import datos.*;
+import domain.Usuario;
+
+import java.sql.*;
+import java.util.List;
+
+public class TestManejoUsuarios {
+    public static void main(String[] args) {
+        Connection conexion = null;
+
+        try {
+            conexion = Conexion.getConnection();
+            if (conexion.getAutoCommit()) {
+                conexion.setAutoCommit(false);
+            }
+
+            UsuarioDAO usuarioDAO = new UsuarioDAO(conexion);
+
+            Usuario updateUsuario = new Usuario(3, "CoscuMarica", "12345");
+            usuarioDAO.update(updateUsuario);
+            Usuario newUsuario = new Usuario("FrodoCrack", "12345");
+            usuarioDAO.insertar(newUsuario);
+
+            conexion.commit();
+            System.out.println("Se ha hecho commit de la transacción");
+
+        } catch (SQLException e) {
+            e.printStackTrace(System.out);
+            System.out.println("Entramos al rollback");
+            try {
+                conexion.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+
+
+    }
 }
